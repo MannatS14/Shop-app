5e9ba66be895f8e1c0af381f7fc2c21bf974f712
Mannat Singh <mannatwadhwa14@gmail.com>
2025-05-02 01:42:13 +0530

    Basic Backend Setup with Node.Js and Linking MongoDB

---
+++ server-side/node-js-startup/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://Mannat:9rk4ljNVmM7zmyZb@shop-app.kotw1e5.mongodb.net/?retryWrites=true&w=majority&appName=Shop-app');
+const db = mongoose.connection;
+db.on('error',(error) => console.error(error));
+db.once('open',() => console.log('Connected to Database'));
+
+app.post('/', (req,res) => {
+    const {name, age, email} = req.body;
+    const newUser = new User({name: name, age: age, email: email});
+    newUser.save();
+    res.json('API is working');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on :${port}`);
+});
+
+
+const { Schema, model } = mongoose;
+const userSchema = new Schema({
+    name: String,
+    age: Number,
+    email: String
+});
+const User = model('User', userSchema);
