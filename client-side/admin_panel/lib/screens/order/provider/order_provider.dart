b467d1e0cb7faab60d5f4b82c71f74b28a7f0688
Mannat Singh <mannatwadhwa14@gmail.com>
2025-05-20 07:44:50 +0530

    Added order page and dashboard leftover

--- client-side/admin_panel/lib/screens/order/provider/order_provider.dart
+++ client-side/admin_panel/lib/screens/order/provider/order_provider.dart
@@ -1,8 +1,11 @@
+import 'package:admin/models/api_response.dart';
+import 'package:admin/utility/snack_bar_helper.dart';
+import 'package:get/get_connect/http/src/response/response.dart';
+
 import '../../../models/order.dart';
 import '../../../services/http_services.dart';
 import 'package:flutter/cupertino.dart';
 import '../../../core/data/data_provider.dart';
-
 
 class OrderProvider extends ChangeNotifier {
   HttpService service = HttpService();
@@ -14,11 +17,65 @@ class OrderProvider extends ChangeNotifier {
 
   OrderProvider(this._dataProvider);
 
-  //TODO: should complete updateOrder
+  Future<void> updateOrder() async {
+    try {
+      if (orderForUpdate != null) {
+        Map<String, dynamic> order = {
+          'trackingUrl': trackingUrlCtrl.text,
+          'orderStatus': selectedOrderStatus,
+        };
 
+        final response = await service.updateItem(
+          endpointUrl: 'orders',
+          itemData: order,
+          itemId: orderForUpdate?.sId ?? '',
+        );
 
-  //TODO: should complete deleteOrder
+        if (response.isOk) {
+          final ApiResponse apiResponse =
+              ApiResponse.fromJson(response.body, null);
 
+          if (apiResponse.success == true) {
+            SnackBarHelper.showSuccessSnackBar('${apiResponse.message}');
+            _dataProvider.getAllOrders(); // Assuming this method is defined
+          } else {
+            SnackBarHelper.showErrorSnackBar(
+                'Failed to update Order: ${apiResponse.message}');
+          }
+        } else {
+          SnackBarHelper.showErrorSnackBar(
+            'Error: ${response.body?['message'] ?? response.statusText}',
+          );
+        }
+      } else {
+        SnackBarHelper.showErrorSnackBar('No order selected for update.');
+      }
+    } catch (e) {
+      print(e);
+      SnackBarHelper.showErrorSnackBar('An error occurred: $e');
+      rethrow;
+    }
+  }
+
+  deleteOrder(Order order) async {
+    try {
+      Response response = await service.deleteItem(
+          endpointUrl: 'orders', itemId: order.sId ?? '');
+      if (response.isOk) {
+        ApiResponse apiResponse = ApiResponse.fromJson(response.body, null);
+        if (apiResponse.success == true) {
+          SnackBarHelper.showSuccessSnackBar('Order Deleted Successfully');
+          _dataProvider.getAllOrders();
+        }
+      } else {
+        SnackBarHelper.showErrorSnackBar(
+            'Error ${response.body?['message'] ?? response.statusText}');
+      }
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
 
   updateUI() {
     notifyListeners();
